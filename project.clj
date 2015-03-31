c54323e52a89696a931319303b64ed5204c001f5
Slava Andreyev <sandreyev@ets.org>
2015-03-31 17:11:36 -0400

    added .gitignore and project.clj

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject krestiky "0.1.0-SNAPSHOT"
+  :description "Krestiky and Noliky"
+  :url "https://github.com/slava92/krestiky"
+  :license {:name "Unlicense"
+            :url "http://unlicense.org"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
