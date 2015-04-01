25ac4c98906f637a991c20bf829e4f70497fd00e
Slava Andreyev <sandreyev@ets.org>
2015-04-01 13:30:14 -0400

    added clojure.core.typed

--- project.clj
+++ project.clj
@@ -4,4 +4,8 @@
   :license {:name "Unlicense"
             :url "http://unlicense.org"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.match "0.3.0-alpha4"]])
+                 [org.clojure/core.match "0.3.0-alpha4"]
+                 [org.clojure/core.typed "0.2.84"]]
+  ;; core.typed repository:
+  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
+  :core.typed {:check [krestiky.Player]})
--- src/krestiky/Player.clj
+++ src/krestiky/Player.clj
@@ -1,14 +1,19 @@
 (ns krestiky.Player
-  (:require [clojure.core.match :refer [match]]))
+  (:require [clojure.core.match :refer [match]]
+            [clojure.core.typed :as t]))
 
-(defprotocol Player
+(t/defprotocol Player
   "A Player"
   (alternate [this] "Returns player that has the next move.")
   (toSymbol [this] "Returns symbol representing the player.")
   (toString [this] "Returns string representing the player."))
 
+(t/ann-datatype player1 [])
 (deftype player1 [])
+
+(t/ann-datatype player2 [])
 (deftype player2 [])
+
 (def Player1 (player1.))
 (def Player2 (player2.))
 
@@ -17,16 +22,16 @@
   (alternate [this] Player2)
   (toSymbol [this] \A)
   (toString [this] "A")
+
   player2
   (alternate [this] Player1)
   (toSymbol [this] \B)
   (toString [this] "B"))
 
+(t/ann valueOf [String -> (t/U player1 player2)])
 (defn valueOf [name]
   (match name
          "A" Player1
-         "B" Player2
-         :else (throw (IllegalArgumentException.
-                       (format "illegal player name %s" name)))))
+         "B" Player2))
 
 (defn values [] [Player1 Player2])
