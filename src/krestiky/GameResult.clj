e4182f6769a61088a158c012b32c2f74c6179d68
Slava <sandreye@pallada.home>
2015-04-03 08:03:47 -0400

    removed polymorphism from GameResult

--- src/krestiky/GameResult.clj
+++ src/krestiky/GameResult.clj
@@ -1,39 +1,59 @@
 (ns krestiky.GameResult
-  (:require [clojure.core.match :refer [match]]
+  (:require [krestiky.Player :refer [Player Player1 Player2]]
+            [clojure.core.match :refer [match]]
             [clojure.core.typed :as t]))
 
-(t/defprotocol GameResult
+(t/defprotocol ;; [t/All [X :variance :covariant]]
+  GameResult
   "Game Result"
-  (isDraw [this] :- boolean)
-  (isWin [this] :- boolean)
-  (strictFold [this p1 p2 dr])
-  (toString [this] :- String)
-  (winner [this] :- (t/U nil t/Any)))
+  (isDraw [this :- GameResult] :- boolean)
+  (isWin [this :- GameResult] :- boolean)
+  ;; <X> X strictFold(X player1Wins, X player2Wins, X draw) 
+  (strictFold [this  :- GameResult
+               p1 :- t/Any p2 :- t/Any dr :- t/Any] :- t/Any)
+  (toString [this :- GameResult] :- String)
+  (winner [this :- GameResult] :- (t/U nil Player)))
 
-(def ^:private win1 :win1)
-(def ^:private win2 :win2)
-(def ^:private draw :draw)
-
+(t/ann Draw GameResult)
 (declare Draw)
+(t/ann Player1Wins GameResult)
 (declare Player1Wins)
+(t/ann Player2Wins GameResult)
 (declare Player2Wins)
 
-(t/ann-datatype position [c :- char i :- int])
+(t/ann-datatype game-result [r :- char])
 (deftype ^:private game-result [r]
          GameResult
-         (isDraw [this] :- boolean)
-         (isWin [this] :- boolean)
-         (strictFold [this p1 p2 dr])
-         (toString [this] :- String)
-         (winner [this] :- (t/U nil t/Any)))
+         (isDraw [this]
+           (match this Draw true :else false))
+         (isWin [this] (match this Draw false :else true))
+         (strictFold [this p1 p2 dr] p1)
+         (toString [this]
+           (match this
+                  Draw "Draw"
+                  Player1Wins "Player 1 Wins"
+                  Player2Wins "Player 2 Wins"))
+         (winner [this]
+           (match this Player1Wins Player1 Player2Wins Player2 :else nil)))
 
-;; boolean	isDraw() 
-;; boolean	isWin() 
-;; <X> X	strictFold(X player1Wins, X player2Wins, X draw) 
-;; java.lang.String	toString() 
-;; fj.data.Option<Player>	winner()
-;; Returns the enum constant of this type with the specified name.
-;; Returns an array containing the constants of this enum type, in the order they are declared.
+(def Draw (->game-result \d))
+(def Player1Wins (->game-result \1))
+(def Player2Wins (->game-result \2))
+
 ;; static GameResult	valueOf(java.lang.String name)
+(t/ann valueOf [String -> GameResult])
+(defn valueOf [name]
+  (match name
+         "Draw" Draw
+         "Player 1 Wins" Player1Wins
+         "Player 2 Wins" Player2Wins))
+
 ;; static GameResult[]	values()
-;; static GameResult	win(Player p) 
+(def values [Draw Player1Wins Player2Wins])
+
+;; static GameResult	win(Player p)
+(t/ann win [Player -> GameResult])
+(defn win [p]
+  (match p
+         Player1 Player1Wins
+         Player2 Player2Wins))
