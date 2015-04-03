c0567f5a7c4670f280730608b1079f1ff3c24456
Slava Andreyev <sandreyev@ets.org>
2015-04-03 11:28:16 -0400

    added polymorphic annotation to strictFold

--- src/krestiky/GameResult.clj
+++ src/krestiky/GameResult.clj
@@ -3,16 +3,15 @@
             [clojure.core.match :refer [match]]
             [clojure.core.typed :as t]))
 
-(t/defprotocol ;; [t/All [X :variance :covariant]]
-  GameResult
+(t/defprotocol
+    GameResult
   "Game Result"
   (isDraw [this :- GameResult] :- boolean)
   (isWin [this :- GameResult] :- boolean)
-  ;; <X> X strictFold(X player1Wins, X player2Wins, X draw) 
-  (strictFold [this  :- GameResult
-               p1 :- t/Any p2 :- t/Any dr :- t/Any] :- t/Any)
+  ([x] strictFold [this  :- GameResult
+                   player1Wins :- x player2Wins :- x draw :- x] :- x)
   (toString [this :- GameResult] :- String)
-  (winner [this :- GameResult] :- (t/U nil Player)))
+  (winner [this :- GameResult] :- (t/Option Player)))
 
 (t/ann Draw GameResult)
 (declare Draw)
@@ -36,11 +35,10 @@
          (winner [this]
            (match this Player1Wins Player1 Player2Wins Player2 :else nil)))
 
-(def Draw (->game-result \d))
+(def Draw (->game-result \D))
 (def Player1Wins (->game-result \1))
 (def Player2Wins (->game-result \2))
 
-;; static GameResult	valueOf(java.lang.String name)
 (t/ann valueOf [String -> GameResult])
 (defn valueOf [name]
   (match name
@@ -48,10 +46,8 @@
          "Player 1 Wins" Player1Wins
          "Player 2 Wins" Player2Wins))
 
-;; static GameResult[]	values()
 (def values [Draw Player1Wins Player2Wins])
 
-;; static GameResult	win(Player p)
 (t/ann win [Player -> GameResult])
 (defn win [p]
   (match p
