95f0ca1a4f7bf6ec306840c71150dacb6b7d50bd
Slava <sandreye@pallada.home>
2015-04-08 20:23:08 -0400

    moving to multi-methods

--- src/krestiky/Board.clj
+++ src/krestiky/Board.clj
@@ -1,35 +1,37 @@
 (ns krestiky.Board
-  (:require [krestiky.BoardLike :refer [BoardLike] :as BL]
+  (:require [krestiky.BoardLike :as BL]
             [krestiky.Position :refer [Position toInt] :as Pos]
             [krestiky.Player :refer [Player alternate]]
             [clojure.core.match :refer [match]]
-            [clojure.core.typed :as t])
+            [clojure.core.typed :as t :refer [check-ns]])
   (:import (clojure.lang APersistentMap)))
 (set! *warn-on-reflection* true)
 (t/defalias TakenBack t/Any)
 (t/defalias MoveResult t/Any)
 
-(t/defprotocol Board
-  "Implementation specific methods"
-  (takeBack [this :- Board] :- TakenBack)
-  (moveTo [this :- Board pos :- Position] :- MoveResult))
+(defmethod BL/isEmpty (type "") [_] false)
 
-(t/ann-datatype
- board-type [nextMove :- Player
-             posMap :- (APersistentMap t/AnyInteger Player)
-             nMoves :- t/AnyInteger
-             before :- (t/Option board-type)])
-(deftype board-type [nextMove posMap nMoves before]
-  BoardLike
-  (isEmpty [this] false)
-  (nmoves [this] nMoves)
-  (occupiedPositions [this]
-    (map (t/fn [pos :- t/AnyInteger] :- Position (Pos/fromInt pos))
-         (keys posMap)))
-  (playerAt [this pos]
-    (get posMap (Pos/toInt pos)))
-  (whoseTurn [this] nextMove)
-  (toString [this] "")
-  Board
-  (takeBack [this] nil)
-  (moveTo [this pos] nil))
+;; (t/defprotocol Board
+;;   "Implementation specific methods"
+;;   (takeBack [this :- Board] :- TakenBack)
+;;   (moveTo [this :- Board pos :- Position] :- MoveResult))
+
+;; (t/ann-datatype
+;;  board-type [nextMove :- Player
+;;              posMap :- (APersistentMap t/AnyInteger Player)
+;;              nMoves :- t/AnyInteger
+;;              before :- (t/Option board-type)])
+;; (deftype board-type [nextMove posMap nMoves before]
+;;   BoardLike
+;;   (isEmpty [this] false)
+;;   (nmoves [this] nMoves)
+;;   (occupiedPositions [this]
+;;     (map (t/fn [pos :- t/AnyInteger] :- Position (Pos/fromInt pos))
+;;          (keys posMap)))
+;;   (playerAt [this pos]
+;;     (get posMap (Pos/toInt pos)))
+;;   (whoseTurn [this] nextMove)
+;;   (toString [this] "")
+;;   Board
+;;   (takeBack [this] nil)
+;;   (moveTo [this pos] nil))
--- src/krestiky/BoardLike.clj
+++ src/krestiky/BoardLike.clj
@@ -2,45 +2,64 @@
   (:require [krestiky.Position :refer [Position toInt] :as Pos]
             [krestiky.Player :refer [Player alternate]]
             [clojure.core.match :refer [match]]
-            [clojure.core.typed :as t]))
+            [clojure.core.typed :as t :refer [check-ns]]))
 (set! *warn-on-reflection* true)
 (t/defprotocol [[x :variance :covariant]] P1 (_1 [this :- (P1 x)] :- x))
 
-(t/defprotocol IBoardLike
-  "A IBoardLike thing"
-  (isEmpty [this :-  IBoardLike] :- boolean)
-  (nmoves [this :- IBoardLike] :- t/AnyInteger)
-  (occupiedPositions [this :- IBoardLike] :- (t/Seqable Position))
-  (playerAt [this :- IBoardLike p :- Position] :- (t/Option Player))
-  (whoseTurn [this :- IBoardLike] :- Player)
-  (isOccupied [tis :- IBoardLike p :- Position] :- boolean)
-  (isNotOccupied [tis :- IBoardLike p :- Position] :- boolean)
-  (playerAtOr [this :- IBoardLike p :- Position or :- (P1 Player)] :- Player)
-  (whoseNotTurn [this :- IBoardLike] :- Player)
-  (toString [this :- IBoardLike
-             af :- (t/IFn [(t/Option Player) Position -> char])] :- String))
+(t/ann-datatype board-like [])
+(deftype board-like [])
 
-(deftype BoardLike []
-  IBoardLike
-  (isEmpty [this] (throw (Exception. "abstract")))
-  (nmoves [this] (throw (Exception. "abstract")))
-  (occupiedPositions [this] (throw (Exception. "abstract")))
-  (playerAt [this p] (throw (Exception. "abstract")))
-  (whoseTurn [this] (throw (Exception. "abstract")))
-  (isOccupied [this p]
-    (not= nil (playerAt this p)))
-  (isNotOccupied [this p]
-    (= nil (playerAt this p)))
-  (whoseNotTurn [this]
-    (alternate (whoseTurn this)))
-  (toString
-    [this af]
-    (let [ps (->> Pos/values (sort-by Pos/toInt)
-                  (map (t/fn [p :- Position] :- char (af (playerAt this p) p)))
-                  (partition-all 3)
-                  (map (t/fn [cs :- (t/Seqable char)] (interpose \space cs)))
-                  (interpose ["\n"]))]
-      (apply str (concat ps)))))
+(t/ann isEmpty [t/Any -> boolean])
+(defmulti isEmpty type)
+(defmethod isEmpty :default [_] (throw (Exception. "abstract")))
+(t/ann nmoves [t/Any -> t/AnyInteger])
+(defmulti nmoves type)
+(defmethod nmoves :default [_] (throw (Exception. "abstract")))
+(t/ann occupiedPositions [t/Any -> (t/Coll Position)])
+(defmulti occupiedPositions type)
+(defmethod occupiedPositions :default [_] (throw (Exception. "abstract")))
+(t/ann playerAt [t/Any Position -> (t/Option Player)])
+(defmulti playerAt (t/fn [x :- t/Any y :- Position] (type x)))
+(defmethod playerAt :default [_ _] (throw (Exception. "abstract")))
+(t/ann whoseTurn [t/Any -> Player])
+(defmulti whoseTurn type)
+(defmethod whoseTurn :default [_] (throw (Exception. "abstract")))
+
+;; (t/defprotocol IBoardLike
+;;   "A IBoardLike thing"
+;;   (isEmpty [this :-  IBoardLike] :- boolean)
+;;   (nmoves [this :- IBoardLike] :- t/AnyInteger)
+;;   (occupiedPositions [this :- IBoardLike] :- (t/Seqable Position))
+;;   (playerAt [this :- IBoardLike p :- Position] :- (t/Option Player))
+;;   (whoseTurn [this :- IBoardLike] :- Player)
+;;   (isOccupied [tis :- IBoardLike p :- Position] :- boolean)
+;;   (isNotOccupied [tis :- IBoardLike p :- Position] :- boolean)
+;;   (playerAtOr [this :- IBoardLike p :- Position or :- (P1 Player)] :- Player)
+;;   (whoseNotTurn [this :- IBoardLike] :- Player)
+;;   (toString [this :- IBoardLike
+;;              af :- (t/IFn [(t/Option Player) Position -> char])] :- String))
+
+;; (deftype BoardLike []
+;;   IBoardLike
+;;   (isEmpty [this] (throw (Exception. "abstract")))
+;;   (nmoves [this] (throw (Exception. "abstract")))
+;;   (occupiedPositions [this] (throw (Exception. "abstract")))
+;;   (playerAt [this p] (throw (Exception. "abstract")))
+;;   (whoseTurn [this] (throw (Exception. "abstract")))
+;;   (isOccupied [this p]
+;;     (not= nil (playerAt this p)))
+;;   (isNotOccupied [this p]
+;;     (= nil (playerAt this p)))
+;;   (whoseNotTurn [this]
+;;     (alternate (whoseTurn this)))
+;;   (toString
+;;     [this af]
+;;     (let [ps (->> Pos/values (sort-by Pos/toInt)
+;;                   (map (t/fn [p :- Position] :- char (af (playerAt this p) p)))
+;;                   (partition-all 3)
+;;                   (map (t/fn [cs :- (t/Seqable char)] (interpose \space cs)))
+;;                   (interpose ["\n"]))]
+;;       (apply str (concat ps)))))
 
 ;; abstract boolean	isEmpty() 
 ;; abstract int	nmoves() 
