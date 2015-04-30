b747d8d7f96be1c1bfc4fa023afa061ba316669c
Slava Andreyev <sandreyev@ets.org>
2015-04-30 17:08:46 -0400

    completed TakeBack.clj

--- src/hrest/Position.clj
+++ src/hrest/Position.clj
@@ -15,6 +15,6 @@
 (def SE (->Position "SE"))
 
 (t/ann positions (t/Coll Position))
-(def positions [NW N NE E SE S SW W C])
+(def positions [NW N NE W C E SE S SW])
 
 (defmethod show Position [p] (:pos p))
--- src/hrest/TakeBack.clj
+++ src/hrest/TakeBack.clj
@@ -1,11 +1,7 @@
 (ns hrest.TakeBack
-  (:require [hrest.Types :refer :all]
-            [hrest.BoardLike :as BL]
-            [hrest.GameResult :as GR]
-            [hrest.MoveResult :as MR]
-            [hrest.Position :as Pos :refer [NW N NE E SE S SW W C]])
-  (:import [hrest.Types EmptyBoard Board FinishedBoard
-            PositionOccupied KeepPlaying GameFinished]
+  (:require [hrest.Types :refer :all])
+  (:import [hrest.Types Board FinishedBoard
+            TakeBackIsEmpty TakeBackIsBoard]
            [hrest.Types Position Player])
   (:require [clojure.core.typed :as t :refer [check-ns]]))
 (set! *warn-on-reflection* true)
@@ -15,17 +11,38 @@
 (t/ann takeBack (t/All [from] [from -> TakenBack]))
 (defmulti takeBack (fn [board] (clazz board)))
 
-;; instance TakeBack FinishedBoard Board where
-;;   takeBack (FinishedBoard (Board ((p, _):t) m) _) =
-;;     Board t (p `M.delete` m)
-;;   takeBack (FinishedBoard (Board [] _) _) =
-;;     error "Broken invariant: board-in-play with empty move list. This is a program bug."
+;; instance TakeBack FinishedBoard TakenBack where
 (defmethod takeBack FinishedBoard [{:keys [b]}]
   (let [pos-plr (first (:moves b))
         pos (t/ann-form (if (nil? pos-plr)
-                          (abstract "empty moves list - bad design")
+                          (abstract "Broken invariant: board-in-play with empty move list. This is a program bug.")
                           (first pos-plr))
                         Position)
         positions' (dissoc (:positions b) pos)
         moves' (apply list (rest (:moves b)))]
     (->TakeBackIsBoard (->Board moves' positions'))))
+
+;; instance TakeBack Board TakenBack where
+(defmethod takeBack Board [board]
+  (if (= 1 (count (:moves board)))
+    (->TakeBackIsEmpty)
+    (let [pos-plr (first (:moves board))
+          pos (t/ann-form (if (nil? pos-plr)
+                            (abstract "Broken invariant: board-in-play with empty move list. This is a program bug.")
+                            (first pos-plr))
+                          Position)
+          positions' (dissoc (:positions board) pos)
+          moves' (apply list (rest (:moves board)))]
+      (->TakeBackIsBoard (->Board moves' positions'))
+      (undefined))))
+
+(t/ann foldTakenBack
+       (t/All [a] [a [Board -> a] TakenBack -> a]))
+(defmulti foldTakenBack
+  (t/fn [a :- t/Any fb :- t/Any tb :- t/Any] (clazz tb)))
+
+(defmethod foldTakenBack TakeBackIsEmpty [e _ tb] e)
+(defmethod foldTakenBack TakeBackIsBoard [_ k tb] (k (:board tb)))
+
+(t/defn takenBackBoard [tb :- TakenBack] :- (t/Option Board)
+  (foldTakenBack nil (t/fn [b :- Board] :- Board b) tb))
