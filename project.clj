b85a4ed421e75be307c0e0a4022f28916d5ae462
Slava Andreyev <sandreyev@ets.org>
2015-05-06 17:04:05 -0400

    warming up

--- project.clj
+++ project.clj
@@ -5,7 +5,26 @@
             :url "http://unlicense.org"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.match "0.3.0-alpha4"]
-                 [org.clojure/core.typed "0.2.84"]]
+                 [org.clojure/core.typed "0.2.84"]
+                 [org.clojure/clojurescript "0.0-3211"]]
   ;; core.typed repository:
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
-  :core.typed {:check [krestiky.Board hrest.Board]})
+  :core.typed {:check [krestiky.Board hrest.Board]}
+  ;; ClojureScript related
+  :plugins [[lein-cljsbuild "1.0.5"]]
+  :cljsbuild {:builds
+              {:app {:source-paths ["srcjs"]
+                     :compiler {:output-to     "out/app.js"
+                                :output-dir    "out"
+                                :asset-path   "js/out"
+                                :optimizations :none
+                                :pretty-print  true}}
+              ;; {:dev
+              ;;  {:source-paths ["srcjs"]
+              ;;   :compiler {:output-to "out/main.js"
+              ;;              :main 'noliky.Types}}
+               ;; :min
+               ;; {:source-paths ["srcjs"]
+               ;;  :compiler {:output-to "out/main.js"
+               ;;             :optimizations :advanced}}
+               }})
