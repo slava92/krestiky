5ea22ff4cdfb9e8c34ee0799f417738afadd209f
Slava Andreyev <slava92@yahoo.com>
2015-05-07 06:45:35 -0400

    trying node.js

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/slava92/krestiky"
   :license {:name "Unlicense"
             :url "http://unlicense.org"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojurescript "0.0-3211"]
+                 [org.clojure/clojure "1.7.0-beta1"]
                  [org.clojure/core.match "0.3.0-alpha4"]
-                 [org.clojure/core.typed "0.2.84"]
-                 [org.clojure/clojurescript "0.0-3211"]]
+                 [org.clojure/core.typed "0.2.84"]]
   ;; core.typed repository:
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :core.typed {:check [krestiky.Board hrest.Board]}
