562fdf22f2d8fc224377176871c0d5f3e6a5463f
Slava Andreyev <sandreyev@ets.org>
2015-06-01 15:05:24 -0400

    added double-check dependency

--- test/cljs/example/core.cljs
+++ test/cljs/example/core.cljs
@@ -1,8 +1,21 @@
 (ns example.core
-  (:require [cemerick.cljs.test :refer-macros (is deftest)]))
+  (:require [cemerick.cljs.test]
+            [clojure.test.check :as sc]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop])
+  (:require-macros [clojure.test.check.clojure-test :refer (defspec)]
+                   [clojure.test.check.properties :refer (for-all)]
+                   [cemerick.cljs.test :refer (is testing deftest test-var
+                                                  run-tests)]))
 
 (deftest somewhat-less-wat
   (is (= "{}[]" (+ {} []))))
 
 (deftest javascript-allows-div0
   (is (= js/Infinity (/ 1 0) (/ (int 1) (int 0)))))
+
+(defspec first-element-is-min-after-sorting ;; the name of the test
+  100 ;; the number of iterations for test.check to test
+  (for-all [v (gen/not-empty (gen/vector gen/int))]
+                (= (apply min v)
+                   (first (sort v)))))
